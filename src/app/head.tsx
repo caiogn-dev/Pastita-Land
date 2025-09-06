25914ecbcaa896d986389323627455414c1c0503
caiogn-dev <caiogn2002@gmail.com>
2025-09-06 15:25:33 -0300

    adicionando gtag

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -9,16 +9,16 @@ export default function Head() {
         href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&family=PT+Sans:wght@400;700&display=swap"
         rel="stylesheet"
       />
-      {/* Google Analytics 4 tag padrão */}
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-MWC3H6H1BJ"></script>
-      <script dangerouslySetInnerHTML={{
-        __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-MWC3H6H1BJ');
-        `
-      }} />
+        {/* Google Analytics 4 tag padrão */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-MWC3H6H1BJ"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-MWC3H6H1BJ');
+          `
+        }} />
     </>
   );
 }
